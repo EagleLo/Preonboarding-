bb4d0441390647f5194740d787bdbbbeace169ea
ElephyChannel <eagle101288@gmail.com>
2023-07-05 10:01:12 -0700

    First Push

---
+++ main.py
@@ -0,0 +1,78 @@
+import os
+
+import pandas as pd
+import seaborn as sn
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from IPython.core.display import display
+from pl_bolts.datamodules import CIFAR10DataModule
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+from pytorch_lightning.loggers import CSVLogger
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.swa_utils import AveragedModel, update_bn
+from torchmetrics.functional import accuracy
+from models import resnet
+import yaml
+
+# Load the config.yaml file
+with open('config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+    
+seed_everything(7)
+
+
+# Step 0: Prepare Dataset
+PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+BATCH_SIZE = 256 if torch.cuda.is_available() else 64
+NUM_WORKERS = int(os.cpu_count() / 2)
+
+train_transforms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.RandomCrop(32, padding=4),
+        torchvision.transforms.RandomHorizontalFlip(),
+        torchvision.transforms.ToTensor(),
+        cifar10_normalization(),
+    ]
+)
+
+test_transforms = torchvision.transforms.Compose(
+    [
+        torchvision.transforms.ToTensor(),
+        cifar10_normalization(),
+    ]
+)
+
+cifar10_dm = CIFAR10DataModule(
+    data_dir=PATH_DATASETS,
+    batch_size=BATCH_SIZE,
+    num_workers=NUM_WORKERS,
+    train_transforms=train_transforms,
+    test_transforms=test_transforms,
+    val_transforms=test_transforms,
+)
+
+model = LitResnet(lr=0.05)
+
+trainer = Trainer(
+    max_epochs=30,
+    accelerator="auto",
+    devices=1 if torch.cuda.is_available() else None,  # limiting got iPython runs
+    logger=CSVLogger(save_dir="logs/"),
+    callbacks=[LearningRateMonitor(logging_interval="step"), TQDMProgressBar(refresh_rate=10)],
+)
+
+trainer.fit(model, cifar10_dm)
+trainer.test(model, datamodule=cifar10_dm)
+
+
+
+# # Access the configuration values
+# model_architecture = config['model']['architecture']
+# learning_rate = config['model']['learning_rate']
+# dataset_path = config['data']['dataset_path']
+# num_epochs = config['training']['num_epochs']
---
+++ models/resnet.py
@@ -0,0 +1,77 @@
+import torchvision
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from IPython.core.display import display
+from pl_bolts.datamodules import CIFAR10DataModule
+from pl_bolts.transforms.dataset_normalizations import cifar10_normalization
+from pytorch_lightning import LightningModule, Trainer, seed_everything
+from pytorch_lightning.callbacks import LearningRateMonitor
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
+from pytorch_lightning.loggers import CSVLogger
+from torch.optim.lr_scheduler import OneCycleLR
+from torch.optim.swa_utils import AveragedModel, update_bn
+from torchmetrics.functional import accuracy
+from models import resnet
+
+def create_model():
+    model = torchvision.models.resnet18(pretrained=False, num_classes=10)
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
+    model.maxpool = nn.Identity()
+    return model
+
+class LitResnet(LightningModule):
+    def __init__(self, lr=0.05):
+        super().__init__()
+
+        self.save_hyperparameters()
+        self.model = create_model()
+
+    def forward(self, x):
+        out = self.model(x)
+        return F.log_softmax(out, dim=1)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        self.log("train_loss", loss)
+        return loss
+
+    def evaluate(self, batch, stage=None):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+
+        acc = accuracy(preds, y)
+
+        if stage:
+            self.log(f"{stage}_loss", loss, prog_bar=True)
+            self.log(f"{stage}_acc", acc, prog_bar=True)
+
+    def validation_step(self, batch, batch_idx):
+        self.evaluate(batch, "val")
+
+    def test_step(self, batch, batch_idx):
+        self.evaluate(batch, "test")
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.SGD(
+            self.parameters(),
+            lr=self.hparams.lr,
+            momentum=0.9,
+            weight_decay=5e-4,
+        )
+        steps_per_epoch = 45000 
+        scheduler_dict = {
+            "scheduler": OneCycleLR(
+                optimizer,
+                0.1,
+                epochs=self.trainer.max_epochs,
+                steps_per_epoch=steps_per_epoch,
+            ),
+            "interval": "step",
+        }
+        return {"optimizer": optimizer, "lr_scheduler": scheduler_dict}
+    
